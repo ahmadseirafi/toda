e8b5614f490931d71323af3f2bb6b9ea1fc527eb
ahmadseirafi <80850729+ahmadseirafi@users.noreply.github.com>
2023-03-31 20:58:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let inputBox = document.getElementById("input-box");
+let listConatiner = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Please Enter Task");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listConatiner.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+
+    }
+
+    inputBox.value = ""
+    saveData();
+
+}
+
+
+listConatiner.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listConatiner.innerHTML);
+}
+
+function showData() {
+    listConatiner.innerHTML = localStorage.getItem("data");
+}
+
+showData();
